29f510f2561a6fde4fb7db734b6171211fc75ec6
Ali Yeşilkanat <aliyesilkanat@gmail.com>
2015-02-08 15:35:59 +0200

    Solved problems on fetcher tests

--- stalker/src/main/java/com/aliyesilkanat/stalker/retriever/Retriever.java
+++ stalker/src/main/java/com/aliyesilkanat/stalker/retriever/Retriever.java
@@ -4,6 +4,7 @@ import java.util.concurrent.ExecutionException;
 import java.util.concurrent.TimeoutException;
 
 import org.apache.log4j.Logger;
+import org.eclipse.jetty.client.HttpClient;
 
 public class Retriever {
 	private final Logger logger = Logger.getLogger(Retriever.class);
@@ -11,7 +12,8 @@ public class Retriever {
 	public String requestDocument(String uri) {
 		String result = null;
 		try {
-			result = ClientSingleton.getInstance().newRequest(uri).send()
+			HttpClient instance = ClientSingleton.getInstance();
+			result = instance.newRequest(uri).send()
 					.getContentAsString();
 		} catch (InterruptedException | TimeoutException | ExecutionException e) {
 			String msg = "error while requesting document {\"uri\":\"%s\"}";
