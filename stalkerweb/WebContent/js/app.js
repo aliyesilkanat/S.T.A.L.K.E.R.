1ab8ae168f274b50b54e4cafd91488d9d6416528
Anil Sevici <anilsevici@hotmail.com>
2015-02-15 18:40:17 +0200

    Implementing web

--- stalkerweb/WebContent/js/app.js
+++ stalkerweb/WebContent/js/app.js
@@ -1,19 +1,20 @@
-var myApp = angular.module("app", [ "ui.router", "HomeCtrl" ]);
-myApp.config(function($stateProvider, $urlRouterProvider) {
-	//
-	// For any unmatched url, redirect to /state1
-	$urlRouterProvider.otherwise("/home");
-	//
-	// Now set up the states
-	$stateProvider.state('home', {
-		url : "/home",
-		templateUrl : "home.html",
-		controller : 'HomeCtrl'
-	}).state('about', {
-		url : "/about",
-		templateUrl : "about.html",
-	}).state('contact', {
-		url : "/contact",
-		templateUrl : "contact.html"
-	});
-});
+var app = angular.module("app", [ "ui.router", "HomeCtrl" ]);
+app.config([ '$stateProvider', '$urlRouterProvider', '$locationProvider',
+		function($stateProvider, $urlRouterProvider, $locationProvider) {
+
+			// For any unmatched url, redirect to /state1
+			$urlRouterProvider.otherwise("/home");
+			//
+			// Now set up the states
+			$stateProvider.state('home', {
+				url : "/home",
+				templateUrl : "home.html",
+				controller : 'HomeCtrl'
+			}).state('about', {
+				url : "/about",
+				templateUrl : "about.html",
+			}).state('contact', {
+				url : "/contact",
+				templateUrl : "contact.html"
+			});
+		} ]);
